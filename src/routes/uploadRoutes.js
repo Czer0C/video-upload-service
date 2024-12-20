da76492dd94f068c478bfca6415a02f0d6917024
Czer0C <trieunguyen04239@gmail.com>
2024-12-20 17:25:40 +0700

    up

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const multer = require('multer')
 const bucket = require('../config/gcs')
+const fs = require('fs')
 
 const router = express.Router()
 const auth = require('../middleware/auth')
+const path = require('path')
 
 // Multer configuration
 const storage = multer.memoryStorage()
@@ -15,29 +17,47 @@ router.post('/', upload.single('video'), async (req, res) => {
       return res.status(400).json({ message: 'No file uploaded' })
     }
 
-    const blob = bucket.file(req.file.originalname)
+    const fileName = req.file.originalname
 
-    const blobStream = blob.createWriteStream({
-      resumable: false,
-      metadata: {
-        contentType: req.file.mimetype,
-      },
+    const outputDir = path.join('processing', fileName)
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true })
+    }
+
+    //! store temporary file for processing service
+    const inputPath = path.join(outputDir, fileName)
+
+    fs.writeFileSync(inputPath, req.file.buffer)
+
+    res.status(200).json({
+      message: 'Processing file, you will be notified when it is done',
+      outputDir,
     })
 
-    blobStream.on('error', (err) =>
-      res.status(500).json({ error: err.message })
-    )
+    // const blob = bucket.file(req.file.originalname)
 
-    blobStream.on('finish', () => {
-      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-      res
-        .status(200)
-        .json({ message: 'File uploaded successfully', url: publicUrl })
-    })
+    // const blobStream = blob.createWriteStream({
+    //   resumable: false,
+    //   metadata: {
+    //     contentType: req.file.mimetype,
+    //   },
+    // })
 
-    blobStream.end(req.file.buffer)
+    // blobStream.on('error', (err) =>
+    //   res.status(500).json({ error: err.message })
+    // )
+
+    // blobStream.on('finish', () => {
+    //   const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    //   res
+    //     .status(200)
+    //     .json({ message: 'File uploaded successfully', url: publicUrl })
+    // })
+
+    // blobStream.end(req.file.buffer)
   } catch (err) {
-    res.status(500).json({ error: 'Server error' })
+    res.status(500).json({ error: err.message })
   }
 })
 
