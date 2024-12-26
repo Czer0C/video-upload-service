09e624286d04bbc5079bf23dbeacdfa5484ea1da
Czer0C <trieunguyen04239@gmail.com>
2024-12-26 17:23:46 +0700

    download

--- src/routes/getRoutes.js
+++ src/routes/getRoutes.js
@@ -5,6 +5,18 @@ const router = express.Router()
 const auth = require('../middleware/auth')
 
 router.get('/', async (req, res) => {
+  const { fileName } = req.query
+
+  if (fileName) {
+    try {
+      const fileInfo = await bucket.file(fileName).getMetadata()
+
+      return res.status(200).json(fileInfo)
+    } catch (error) {
+      return res.status(error.code).json({ message: error.message })
+    }
+  }
+
   const [, , listFiles] = await bucket.getFiles()
 
   const finalListFiles =
@@ -13,15 +25,31 @@ router.get('/', async (req, res) => {
   res.status(200).json(finalListFiles)
 })
 
-router.get('/:fileName', async (req, res) => {
-  const { fileName } = req.params
+router.get('/download', async (req, res) => {
+  const { fileName } = req.query
+
+  if (!fileName) {
+    return res.status(400).json({ message: 'File name is required' })
+  }
 
   try {
-    const fileInfo = await bucket.file(fileName).getMetadata()
+    const file = bucket.file(fileName)
 
-    res.status(200).json(fileInfo)
+    const fileExists = await file.exists()
+
+    if (!fileExists[0]) {
+      return res.status(404).json({ message: 'File not found' })
+    }
+
+    const readStream = file.createReadStream()
+
+    readStream.on('error', (err) => {
+      res.status(500).json({ message: err.message })
+    })
+
+    readStream.pipe(res)
   } catch (error) {
-    res.status(error.code).json({ message: error.message })
+    return res.status(error.code).json({ message: error.message })
   }
 })
 
